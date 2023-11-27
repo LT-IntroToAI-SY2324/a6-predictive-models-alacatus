10b15bc6bf38d8a65bffa32fa2379d0117d4726e
alacatus <alacatus@cps.edu>
2023-11-27 11:28:52 -0600

    done with blood pressure LR model

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -10,15 +10,44 @@ y = data["Blood Pressure"].values
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
-# Create the model
+# create the model
+model = LinearRegression().fit(x, y)
 
-# Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
+# find the coefficient, bias, and r squared values
+# each should be a float and rounded to two decimal places
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
 
+# predict blood pressure of someone who is 43 yeras old
+x_predict = 43
+# plug that value into your model
+prediction = model.predict([[x_predict]])
 
-# Print out the linear equation and r squared value
+# print out the linear equation and r squared value
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
 
-# Predict the the blood pressure of someone who is 43 years old.
-# Print out the prediction
+'''
+The following code creates the graph to visualize the data
+'''
+# sets the size of the graph
+plt.figure(figsize=(6,4))
 
-# Create the model in matplotlib and include the line of best fit
+# creates a scatter plot of originial data in purple
+# and the predicted data in blue
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+# label the axes
+plt.xlabel("Age")
+plt.ylabel("Systolic Blood Pressure")
+plt.title("Systolic Blood Pressure by Age")
+
+# plot the line of best fit in red and label the line
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+# show the plot and legend
+plt.legend()
+plt.show()
